30b71bad2c4902b522361ea2e9774126b0814122
TheHockeyist <thockeyist@gmail.com>
2017-08-20 20:39:37 -0500

    Just a brief message.

---
+++ message.py
@@ -0,0 +1,11 @@
+So guys, I'm not dead. I've just been handling summer. And I've lost a lot of interest from Github. :-/.
+
+Also, I've been planning for 2018, and I plan to perhaps be on here more active,
+but I already have ten New Year's resolutions for 2018 and it's not even September.
+
+I may or may not continue here, but if I don't, I have my own best interests at heart. I may learn Python in 2018, I may not.
+I already have a lot of things planned out for 2018, so if I'm inactive on Github through 2018, you can blame me and my resolutions.
+
+Perhaps I'll start comics or animations. I don't know yet. I have a life outside of here.
+
+Also, I'm starting college next week, so Github will be neglected. Wish me luck.
